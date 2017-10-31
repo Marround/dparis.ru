7054598a63310cd70a7b9c9fdf4faae741539793
Marround <marround@yandex.ru>
2017-10-31 16:36:34 +0300

    etc update 31-10-2017

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {RouterModule} from "@angular/router";
+import {SectionsModule, routs} from "./sections/index";
+import { MenuComponent } from './menu/menu.component';
+import {MatIconModule, MatButtonModule} from "@angular/material";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(routs),
+    BrowserModule,
+    BrowserAnimationsModule,
+    SectionsModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/sections/sections.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule} from '@angular/common';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatCardModule, MatIconModule, MatListModule, MatTabsModule} from '@angular/material';
+
+import { HomeSectionComponent } from './home/home.component';
+import { PricelistSectionComponent} from './pricelist/pricelist.component';
+import { AboutusSectionComponent } from './aboutus/aboutus.component';
+import { ContactsSectionComponent } from './contacts/contacts.component';
+
+@NgModule({
+  imports: [CommonModule, BrowserAnimationsModule, MatCardModule, MatTabsModule, MatListModule, MatIconModule],
+  declarations: [HomeSectionComponent, PricelistSectionComponent, AboutusSectionComponent, ContactsSectionComponent],
+  exports: [HomeSectionComponent, PricelistSectionComponent, AboutusSectionComponent, ContactsSectionComponent]
+})
+
+export class SectionsModule {
+
+}
